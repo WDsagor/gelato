a0682b58d93008624281a3d3a1fa4854d3507d81
Wdsagor <sagarwpress@gmail.com>
2024-09-14 22:56:10 +0600

    Project init

---
+++ Components/Share/TopNavbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopNavbar = () => {
+    return (
+        <div className='bg-black py-2'>
+            <p className='text-white text-center font-light'>FREE SHIPPING for orders over $300. Order today</p>
+        </div>
+    );
+};
+
+export default TopNavbar;
